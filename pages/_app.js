4fc9a4e1cad2c1001c5a7c56e77c02e91c372975
Colin <colin.diesh@gmail.com>
2021-09-05 21:14:56 -0400

    Attempt deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -2,7 +2,7 @@ export default function Layout({ children }) {
   return (
     <div>
       <a href="/">Return home</a>
-      <div style={{ margin: "0 auto", width: "75%" }}>{children}</div>
+      <div className="blog">{children}</div>
     </div>
   );
 }
