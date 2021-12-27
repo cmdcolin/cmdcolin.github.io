5f7ce2aec4637198363b9aa769d1bb46b4b23c08
Colin <colin.diesh@gmail.com>
2021-12-26 20:17:17 -0700

    Add spacing in footer links

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,29 +4,27 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer style={{ marginTop: 20 }}>
-      <Container>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        <Link href="/archive">
-          <a>Blog archive</a>
-        </Link>{' '}
-        <Link href="https://github.com/cmdcolin">
-          <a>Github</a>
-        </Link>{' '}
-        <Link href="https://twitter.com/cmdcolin">
-          <a>Twitter</a>
-        </Link>{' '}
-        <Link href="/projects">
-          <a>Projects</a>
-        </Link>{' '}
-        <Link href="/photos">
-          <a>Photos</a>
-        </Link>{' '}
-        <Link href="/rss.xml">
-          <a>RSS</a>
-        </Link>
-      </Container>
+      <Link href="/">
+        <a>Home</a>
+      </Link>{' '}
+      <Link href="/archive">
+        <a>Blog archive</a>
+      </Link>{' '}
+      <Link href="https://github.com/cmdcolin">
+        <a>Github</a>
+      </Link>{' '}
+      <Link href="https://twitter.com/cmdcolin">
+        <a>Twitter</a>
+      </Link>{' '}
+      <Link href="/projects">
+        <a>Projects</a>
+      </Link>{' '}
+      <Link href="/photos">
+        <a>Photos</a>
+      </Link>{' '}
+      <Link href="/rss.xml">
+        <a>RSS</a>
+      </Link>
     </footer>
   )
 }
