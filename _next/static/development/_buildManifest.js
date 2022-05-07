9cafe947e7bffcab43da4df5bd3f9ab9937c0439
Colin <colin.diesh@gmail.com>
2022-05-06 21:47:55 -0600

    Updates

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/kitty":["static\u002Fchunks\u002Fpages\u002Fkitty.js"],"/photos":["static\u002Fchunks\u002Fpages\u002Fphotos.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fkitty","\u002Fphotos"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
