3a4272ff88b3d38930ede3b650b8faf8dc66aca7
Colin <colin.diesh@gmail.com>
2024-11-19 09:18:46 -0500

    Add m-auto back

--- components/container.tsx
+++ components/container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="lg:w-1/2">{children}</div>
+  return <div className="lg:w-1/2 m-auto">{children}</div>
 }
