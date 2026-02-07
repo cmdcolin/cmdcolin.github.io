693f437dbdf30ce2646b3bec2c48e6aff3b01c8a
Colin <colin.diesh@gmail.com>
2026-02-07 18:19:45 -0500

    Highlight treesitter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import rehypeTreeSitter from './lib/rehype-tree-sitter.js'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
   site: 'https://cmdcolin.github.io',
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [rehypeTreeSitter],
+  },
 })
